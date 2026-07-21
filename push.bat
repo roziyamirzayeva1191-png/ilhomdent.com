9f9a5ad0a96b9c0d16082dc2557996b4fb03dc6c
Ulug <roziyamirzayeva1191@gmail.com>
2026-07-21 23:16:10 +0500

    Create push.bat

---
+++ push.bat
@@ -0,0 +1,7 @@
+@echo off
+cd /d "%~dp0"
+echo Ilhomdent kodini GitHub'ga yuklayapman...
+git push origin main
+echo.
+echo Tugadi. Xato bo'lsa, GitHub login/parolni (yoki token) so'rashi mumkin.
+pause
